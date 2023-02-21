12f3ef9ac47471e574db41dcf3af5b7f08c42d36
204incs <120594199+204incs@users.noreply.github.com>
2023-02-21 18:16:15 -0500

    project Outline
    Created Outline to follow

--- CKProject/Program.cs
+++ CKProject/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.Metrics;
+
+
+namespace CKProject
+{
+    class Program 
+       
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Reads in an Input file, parses as a string array
+            //TODO: Reads in a blacklist file and puts it into a list 
+            //TODO: Checks the Validity of an IP address through the regular expression (if not a valid IP, will toss an invalid IP warning message) 
+            //TODO: Checks Valid IP address against a blacklist of IPs. If IP is not valid will give a warning message of blacklist IP message detected) 
+            //TODO: generate a Log that shows all valid IP addresses and ones listed as invalid and blacklisted.
+            // RQ Read data from an external file, such as text, JSON, CSV, etc, and use that data in your application / Implement a regular expression 
+            // RQ Implement a regular expression 
+            // RQ Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program
+            // RQ Read data from an external file, such as text, JSON, CSV, etc., 
+        }
+    }
+}
+  
